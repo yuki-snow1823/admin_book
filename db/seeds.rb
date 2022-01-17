e32f9de66b48e10b984c4e1927f89413d1e67040
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-01-17 22:36:42 +0900

    [add] サンプルデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..5).each do |i|
+  Book.create(
+    name: "Book #{i}",
+    published_on: Time.parse("20191224").ago(i.months),
+    price: (i* 1000)
+  )
+end
