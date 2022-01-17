c3ded6f19cfcdfd465ec65ef76eb15e435de8a9d
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-01-17 23:35:35 +0900

    [add] 著者と出版社の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_133448) do
+ActiveRecord::Schema.define(version: 2022_01_17_143258) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "penname"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.date "published_on"
     t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.string "string"
+    t.string "address"
+    t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
