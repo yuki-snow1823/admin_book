136d6f204a2cf8a797fb3b51180d294cc3427b6b
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-01-17 23:22:59 +0900

    [add] 引数を用いたスコープの定義の学び

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
   # costyというメソッドチェーンが使えるようになる
   scope :costy, -> { where("price >?", 3000) }
+  scope :written_about, ->(theme) { where("name like ?", "%#{theme}%")}
 end
