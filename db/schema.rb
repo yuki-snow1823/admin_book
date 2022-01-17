01fdaacc6faaea89205dfabd0d963ecdb1c36f8e
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-01-17 23:46:41 +0900

    [add] 本と出版社にリレーション作成して、DBの作成し直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_143258) do
+ActiveRecord::Schema.define(version: 2022_01_17_143857) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2022_01_17_143258) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "publisher_id", null: false
+    t.index ["publisher_id"], name: "index_books_on_publisher_id"
   end
 
   create_table "publishers", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2022_01_17_143258) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "books", "publishers"
 end
