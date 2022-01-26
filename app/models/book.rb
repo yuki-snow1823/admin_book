e481e88a66728d82f1e583e216df5feab1d49d02
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-01-27 07:20:48 +0900

    [add] バリデーション

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,13 @@
 class Book < ApplicationRecord
   # costyというメソッドチェーンが使えるようになる
-  scope :costy, -> { where("price >?", 3000) }
-  scope :written_about, ->(theme) { where("name like ?", "%#{theme}%")}
+  scope :costy, -> { where('price >?', 3000) }
+  scope :written_about, ->(theme) { where('name like ?', "%#{theme}%") }
 
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_eaual_to: 0 }
 end
