2761fdf4d9a90724ebe230f777ac1f97dda11bff
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-02-10 20:34:33 +0900

    add: 複雑なルーティングの確認

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/books/:id" => "books#show"
   delete "books/:id" => "books#destroy"
+
+  resources :publishers
+
+  # resources :publishers do
+  #   resources :books
+  #   member do # 小ベルの適用
+  #     get 'detail'
+  #   end
+  #   collection do # 全体への動き
+  #     get 'search'
+  #   end
+  # end
 end
