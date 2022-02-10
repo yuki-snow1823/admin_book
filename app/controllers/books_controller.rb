020d35d3a6d4f7c68f9d89b4ee0a0ff8719a68df
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-02-10 20:27:30 +0900

    destroyアクションとルーティングの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,26 @@
 class BooksController < ApplicationController
+  protect_from_forgery except:[:destroy]
+  before_action :set_book, only:[:show, :destroy]
+
+  def show
+    @book = Book.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json # jsonのリクエストがあればそっちを返す
+    end
+  end
+
+  def destroy
+    @book.destroy
+    respond_to do |format|
+      format.html {redirect_to "/" }
+      format.json {head :no_content }
+    end
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
 end
