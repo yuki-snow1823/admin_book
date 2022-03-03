2da94864d7ac1b9db0ad2edac33a8b27fb2ab0da
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-03-03 17:56:03 +0900

    [add] テスト前後での処理

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -4,4 +4,17 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @book = books(:one)
+    puts "Set up"
+  end
+
+  test "should get index" do
+    # get "/books/1"
+    # assert_response :success
+  end
+
+  teardown do
+    puts "call teardown"
+  end
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    @book = books(:one) # fixtures呼び出し
+    assert_match("MyString", @book.name)
+  end
 end
